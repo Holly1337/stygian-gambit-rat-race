5e9cdee3cecf2af5aa3a704cb61f94492719c6d0
Holly1337 <thomas.schaefer1337@gmx.de>
2024-01-17 17:43:11 +0100

    Adjust vite config for GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://holly1337.github.io/stygian-gambit-rat-race/'
 })
